ccd1e09209f1c1aefd3fcbe5a24cf0ad01667473
Jacob Albano <jacob.albano@gmail.com>
2021-11-28 17:38:35 -0500

    Initial setup

---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCodeScaffolding;
+using System;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new AdventOfCodeApp();
+            app.Run();
+        }
+    }
+}
