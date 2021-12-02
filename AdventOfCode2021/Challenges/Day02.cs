8a31aa2a59cad3835265b3533bdd543a2dc1f6fe
Jacob Albano <jacob.albano@gmail.com>
2021-12-02 07:12:03 -0500

    Day 2 + added util stuff from last year

---
+++ AdventOfCode2021/Challenges/Day02.cs
@@ -0,0 +1,74 @@
+﻿using AdventOfCode2021.Util;
+using AdventOfCodeScaffolding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Challenges
+{
+    [Challenge(2, "Dive!")]
+    class Day02 : ChallengeBase
+    {
+        public override void Part1Test()
+        {
+            Assert.AreEqual(150, Part1(sample));
+        }
+
+        public override void Part2Test()
+        {
+            Assert.AreEqual(900, Part2(sample));
+        }
+
+        public override object Part1(string input)
+        {
+            int x = 0, y = 0;
+            foreach (var (dir, number) in Parse(input))
+            {
+                switch (dir)
+                {
+                    case "up": y -= number; break;
+                    case "down": y += number; break;
+                    case "forward": x += number; break;
+                    default: Assert.Unreachable(); break;
+                }
+            }
+
+            return x * y;
+        }
+
+        public override object Part2(string input)
+        {
+            int x = 0, y = 0, aim = 0;
+            foreach (var (dir, number) in Parse(input))
+            {
+                switch (dir)
+                {
+                    case "up": aim -= number; break;
+                    case "down": aim += number; break;
+                    case "forward":
+                        x += number;
+                        y += aim * number;
+                        break;
+                    default: Assert.Unreachable(); break;
+                }
+            }
+            return x * y;
+        }
+
+        private static IEnumerable<(string, int)> Parse(string input)
+        {
+            return input.ToLines()
+                .Select(x => x.Split(' '))
+                .Select(x => (x[0], int.Parse(x[1])));
+        }
+
+        private const string sample = @"forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+    }
+}
---
+++ AdventOfCode2021/Util/EnumerableExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<IEnumerable<T>> PartitionBy<T>(this IEnumerable<T> self, Func<T, bool> delimit)
+    {
+        using var e = self.GetEnumerator();
+        while (e.MoveNext())
+            yield return Inner();
+
+        IEnumerable<T> Inner()
+        {
+            do
+            {
+                if (delimit(e.Current))
+                    yield break;
+
+                yield return e.Current;
+            } while (e.MoveNext());
+        }
+    }
+
+    public static IEnumerable<IEnumerable<T>> ChunkBy<T>(this IEnumerable<T> self, int chunkSize)
+    {
+        using var e = self.GetEnumerator();
+        while (e.MoveNext())
+            yield return Inner();
+
+        IEnumerable<T> Inner()
+        {
+            for (int i = 0; i < chunkSize; i++)
+            {
+                yield return e.Current;
+                e.MoveNext();
+            }
+        }
+    }
+
+    public static IEnumerable<T> As<T>(this IEnumerable untyped)
+    {
+        foreach (var x in untyped)
+            yield return (T)x;
+    }
+
+    public delegate (bool, TOut) SelectWhereSelector<TIn, TOut>(TIn input);
+
+    public static IEnumerable<TOut> SelectWhere<TIn, TOut>(this IEnumerable<TIn> self, SelectWhereSelector<TIn, TOut> selector)
+    {
+        foreach (var x in self)
+        {
+            var (success, result) = selector(x);
+            if (success) yield return result;
+        }
+    }
+
+    public static IEnumerable<IEnumerable<T>> CartesianProduct<T>(this IEnumerable<IEnumerable<T>> sequences)
+    {
+        IEnumerable<IEnumerable<T>> result = new[] { Enumerable.Empty<T>() };
+        foreach (var sequence in sequences)
+        {
+            var localSequence = sequence;
+            result = result.SelectMany(
+              _ => localSequence,
+              (seq, item) => seq.Concat(new[] { item })
+            );
+        }
+        return result;
+    }
+}
